65cd95500ef2858b73ea02f3dc766ca3c73e6913
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-07-18 10:56:46 +0800

    connected mongoose to mongo

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+const keys = require("./config/keys.js");
+
+mongoose.connect(keys.mongoURI); // connecting Mongoose to MongoDB
 const app = express(); // initiates a new Express app
 
 app.get("/", (req, res) => {
