2933226a0112e2c6844f9e2866d2a9890798ac7a
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-07-23 08:06:18 +0800

    bodyParser middleware added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const keys = require("./config/keys.js");
 
@@ -10,6 +11,7 @@ const Task = mongoose.model("tasks"); // accessing "task" model class
 
 mongoose.connect(keys.mongoURI); // connecting Mongoose to MongoDB
 const app = express(); // initiates a new Express app
+app.use(bodyParser.json()); // passes app into BodyParser middleware
 
 // Importing routes
 require("./routes/taskRoutes.js")(app);
