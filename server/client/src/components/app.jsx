19f0e28ff876c638d389a61dcee1e5ba408d4dd7
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-07-25 21:29:52 +0800

    placeholder routes added

--- server/client/src/components/app.jsx
+++ server/client/src/components/app.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+import * as actions from "../actions";
+
+import Tasks from "./tasks.jsx";
+import New from "./new.jsx";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <BrowserRouter>
+          <Route path="/" exact component={Tasks} />
+          <Route path="/new" component={New} />
+        </BrowserRouter>
       </div>
     );
   };
