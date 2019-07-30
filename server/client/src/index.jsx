6f281d1a2bcdbbf806fcbd46cd87af8bfde66cd2
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-07-31 07:34:24 +0800

    client-side setup done

--- server/client/src/index.jsx
+++ server/client/src/index.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/app.jsx";
 import "./stylesheets/application.scss";
 
+import tasksReducer from "./reducers/tasks_reducer.js";
+
+const reducers = combineReducers({
+  tasks: tasksReducer
+});
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
