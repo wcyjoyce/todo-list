9291e72e7199ace50c245c53547d7dabbf413147
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-08-05 08:13:36 +0800

    deleteTask done

--- server/client/src/reducers/tasks_reducer.js
+++ server/client/src/reducers/tasks_reducer.js
@@ -1,9 +1,12 @@
-import { FETCH_TASKS } from "../actions/types.js";
+import { FETCH_TASKS, DELETE_TASK } from "../actions/types.js";
 
 const tasksReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_TASKS: {
       return action.payload;
+    }
+    case DELETE_TASK: {
+      return state.filter(task => task._id !== action.payload);
     }
     default:
       return state;
